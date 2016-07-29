ca6264766a121a022a2b02d972d925c49aa9eb24
Linh Nguyen <nvlpfiev@gmail.com>
2016-07-29 15:43:55 +0200

    Remove some unused files

--- isotropic/dictionarylearning/stats/POD.m
+++
@@ -1,9 +0,0 @@
-function [D, A, lambda] = POD(X)
-    C = (X'*X)./(size(X,1)-1);                  %'# cov(X)
-
-    [D, lambda] = eig(C);
-    [lambda, order] = sort(diag(lambda), 'descend');       %# sort cols high to low
-    D = D(:,order);
-
-    A = X*D(:,1:end);
-end
